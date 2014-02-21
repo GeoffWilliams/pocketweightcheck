f09ee074f02b22661e179e8c50786fcbd01895f2
Geoff Williams <geoff@geoffwilliams.me.uk>
2014-02-22 03:35:08 +1100

    test ci

--- src/test/java/uk/me/geoffwilliams/pocketweightcheck/MainActivityTest.java
+++ src/test/java/uk/me/geoffwilliams/pocketweightcheck/MainActivityTest.java
@@ -34,6 +34,6 @@ public class MainActivityTest {
     assertTrue(activity != null);
     
     TextView mytextview = (TextView) activity.findViewById(R.id.mytextview);
-    assertEquals("FU JAVA!!!", mytextview.getText().toString());
+    assertEquals("ci test", mytextview.getText().toString());
   }
 }
