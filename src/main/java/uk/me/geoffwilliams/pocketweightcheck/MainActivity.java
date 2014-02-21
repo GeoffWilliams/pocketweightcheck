3907329480e740826fb2e6ff1dee43277ba04ff9
Geoff Williams <geoff@geoffwilliams.me.uk>
2014-02-22 02:34:10 +1100

    working unit tests and skeleton app; licensing added

---
+++ src/main/java/uk/me/geoffwilliams/pocketweightcheck/MainActivity.java
@@ -0,0 +1,40 @@
+/*
+ * pocketweightcheck -- simple android app to track of your weight
+ * Copyright (C) 2014 Geoff Williams <geoff@geoffwilliams.me.uk>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package uk.me.geoffwilliams.pocketweightcheck;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ src/test/java/uk/me/geoffwilliams/pocketweightcheck/MainActivityTest.java
@@ -0,0 +1,39 @@
+/*
+ * pocketweightcheck -- simple android app to track of your weight
+ * Copyright (C) 2014 Geoff Williams <geoff@geoffwilliams.me.uk>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package uk.me.geoffwilliams.pocketweightcheck;
+
+import android.app.Activity;
+import android.widget.TextView;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+  @org.junit.Test
+  public void testSomething() throws Exception {
+    Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
+    assertTrue(activity != null);
+    
+    TextView mytextview = (TextView) activity.findViewById(R.id.mytextview);
+    assertEquals("FU JAVA!!!", mytextview.getText().toString());
+  }
+}
